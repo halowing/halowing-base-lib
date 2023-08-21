8f8e9c6b2d20d8ded15a239a5fc75b9834af5d47
sgkim <sgkim@halowing.com>
2023-08-22 00:00:07 +0900

    DefaultApplicationException 제작.

--- halowing-base-lib/src/main/java/com/halowing/lib/exception/DefaultApplicationException.java
+++ halowing-base-lib/src/main/java/com/halowing/lib/exception/DefaultApplicationException.java
@@ -4,19 +4,19 @@ package com.halowing.lib.exception;
  * It is a RuntimeException Wrapper.
  * It is used to handle that do not use ResourceBundle.
  */
-public class SimpleRuntimeException extends RuntimeException {
+public class DefaultApplicationException extends RuntimeException {
 	
 	private static final long serialVersionUID = 4793274607807687071L;
 
-	public SimpleRuntimeException(Throwable cause) {
+	public DefaultApplicationException(Throwable cause) {
 		super(cause);
 	}
 	
-	public SimpleRuntimeException(String message, Throwable cause) {
+	public DefaultApplicationException(String message, Throwable cause) {
 		super(message, cause);
 	}
 	
-	public SimpleRuntimeException(String message) {
+	public DefaultApplicationException(String message) {
 		super(message);
 	}
 
